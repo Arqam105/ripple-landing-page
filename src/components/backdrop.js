7332a5c091a0a7eb97baae11a962295236e593ed
Arqam Saeed <arqam105@outlook.com>
2020-10-06 15:33:44 +0500

    minor fixes

---
+++ src/components/backdrop.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+import Background from "../images/background.jpg"
+import WaterEffect from "./water-effect"
+import "./backdrop.css"
+
+const Backdrop = () => {
+  const containerStyle = {
+    zIndex: "-1",
+    overflow: "hidden"
+  }
+
+  const [isFront, setIsFront] = useState(false)
+
+  useEffect(() => {
+    if (window ?? false) {
+      setIsFront(true)
+    }
+  }, [])
+
+  if (!isFront) return null
+
+  return (
+    <div style={containerStyle}>
+      <WaterEffect
+        style={{
+          height: "100vh",
+          background: `url(${Background})`,
+          backgroundPosition: "0px 0px"
+        }}
+        className="backdrop"
+        dropRadius={20}
+        perturbance={0.0001} //intensity of animation/waves
+        interactive={true}
+        resolution={512} //animation speed
+      >
+        {({ drop }) => <button onClick={() => drop(50, 50, 20, 0.05)} />}
+      </WaterEffect>
+    </div>
+  )
+}
+
+export default Backdrop
