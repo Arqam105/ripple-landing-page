974a279c97b00df9edd17d9cfff7178c20bb7a1f
Arqam Saeed <arqam105@outlook.com>
2020-10-20 04:55:29 +0500

    logo resolution fixed

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import Logo from "../../images/logo.png"
+import Logo from "../../images/living-skin-logo.png"
 import "./content.css"
 
 function Content(props) {
